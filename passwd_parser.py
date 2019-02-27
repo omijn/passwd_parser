47d685a85fb730e77aafbe28ef81e3a836b6cc0d
omijn <abhijit.kashyap07@gmail.com>
2019-02-27 15:56:08 -0800

    improved file format validation and test cases

--- passwd_parser.py
+++ passwd_parser.py
@@ -1,70 +1,77 @@
 import argparse
 import json
-import os
+import sys
 
 
 class PasswdFieldEnum(object):
     USERNAME = 0
     UID = 2
     GECOS = 4
+    TOTAL_FIELDS = 7
 
 
 class GroupFieldEnum(object):
     GROUPNAME = 0
     USERS = 3
+    TOTAL_FIELDS = 4
 
 
 class FileFormatError(Exception):
     pass
 
 
-# FileNotFoundError and PermissionError aren't available in Python 2, so define them
-try:
-    FileNotFoundError
-except NameError:
-    FileNotFoundError = IOError
-
-try:
-    PermissionError
-except NameError:
-    PermissionError = IOError
-
-
-def verify_file_readable(filepath):
-    if not os.path.exists(filepath):
-        raise FileNotFoundError("File '{}' not found.".format(filepath))
-    if not os.access(filepath, os.R_OK):
-        raise PermissionError("Insufficient read permissions for file '{}'".format(filepath))
-
-
-def validate_passwd_format(user_data, line_num):
-    if len(user_data) != 7:
+def validate_passwd_format(user_data, line_num, users_encountered):
+    if len(user_data) != PasswdFieldEnum.TOTAL_FIELDS:
         raise FileFormatError(
-            "There was an error parsing line {} of the passwd file '{}'.".format(line_num, passwd_path))
+            "Couldn't parse line {} of '{}': expected {} values but found {}".format(line_num,
+                                                                                     passwd_path,
+                                                                                     PasswdFieldEnum.TOTAL_FIELDS,
+                                                                                     len(user_data)))
+
+    if user_data[PasswdFieldEnum.USERNAME] in users_encountered:
+        raise FileFormatError(
+            "Couldn't parse line {} of '{}': duplicate user '{}' found".format(line_num,
+                                                                               passwd_path,
+                                                                               user_data[PasswdFieldEnum.USERNAME]))
 
 
-def validate_group_format(group_data, line_num):
-    if len(group_data) != 4:
-        raise FileFormatError("There was an error parsing line {} of the group file '{}'.".format(line_num, group_path))
+def validate_group_format(group_data, line_num, groups_encountered):
+    if len(group_data) != GroupFieldEnum.TOTAL_FIELDS:
+        raise FileFormatError(
+            "Couldn't parse line {} of '{}': expected {} values but found {}".format(line_num,
+                                                                                     passwd_path,
+                                                                                     GroupFieldEnum.TOTAL_FIELDS,
+                                                                                     len(group_data)))
+
+    if group_data[GroupFieldEnum.GROUPNAME] in groups_encountered:
+        raise FileFormatError(
+            "Couldn't parse line {} of '{}': duplicate group '{}' found".format(line_num,
+                                                                                group_path,
+                                                                                group_data[GroupFieldEnum.GROUPNAME]))
 
 
-def parse_passwd_file(passwd_path, user2groups):
+def parse_passwd_file(passwd_file, user2groups):
     """
     Create a dictionary that contains all the user information we need
 
-    :param passwd_path: path (string) to the passwd file
+    :param passwd_file: path (string) to the passwd file
     :param user2groups: a dictionary that maps usernames to groups
     :return: a dictionary containing user and group information
     """
 
     parsed_data = {}
+    users_encountered = set()
 
-    with open(passwd_path) as passwd_file:
+    with passwd_file:
         for index, user_data in enumerate(passwd_file.read().splitlines()):
             user_data = user_data.split(":")
-            validate_passwd_format(user_data, index + 1)
+            try:
+                validate_passwd_format(user_data, index + 1, users_encountered)
+            except Exception as e:
+                sys.exit(str(e))
 
             username = user_data[PasswdFieldEnum.USERNAME]
+            users_encountered.add(username)
             uid = user_data[PasswdFieldEnum.UID]
             gecos = user_data[PasswdFieldEnum.GECOS]
             fullname = gecos.split(",")[0]
@@ -79,48 +86,61 @@ def parse_passwd_file(passwd_path, user2groups):
     return parsed_data
 
 
-def parse_group_file(group_path):
+def parse_group_file(group_file):
     """
     Create a mapping from users to groups
 
-    :param group_path: path (string) to the group file
+    :param group_file: path (string) to the group file
     :return: a dictionary with usernames as keys and lists of groups as values
     """
 
     user2groups = {}
+    groups_encountered = set()
 
-    with open(group_path) as group_file:
+    with group_file:
         for index, group_data in enumerate(group_file.read().splitlines()):
             group_data = group_data.split(":")
-            validate_group_format(group_data, index + 1)
+            try:
+                validate_group_format(group_data, index + 1, groups_encountered)
+            except Exception as e:
+                sys.exit(str(e))
 
+            group_name = group_data[GroupFieldEnum.GROUPNAME]
+            groups_encountered.add(group_name)
             users = group_data[GroupFieldEnum.USERS]
             if users == '':
                 continue
             for user in users.split(","):
                 if user not in user2groups:
                     user2groups[user] = []
-                else:
-                    user2groups[user].append(group_data[GroupFieldEnum.GROUPNAME])
+
+                user2groups[user].append(group_name)
 
     return user2groups
 
 
-parser = argparse.ArgumentParser(description='Parse the passwd and group files and combine the data into a single JSON output.')
-parser.add_argument("-p", "--passwd", default='/etc/passwd', help='Path to the passwd file. Defaults to /etc/passwd if not provided.')
-parser.add_argument("-g", "--group", default='/etc/group', help='Path to the group file. Defaults to /etc/group if not provided.')
+parser = argparse.ArgumentParser(
+    description='Parse the passwd and group files and combine the data into a single JSON output.')
+parser.add_argument("-p", "--passwd", default='/etc/passwd',
+                    help='Path to the passwd file. Defaults to /etc/passwd if not provided.')
+parser.add_argument("-g", "--group", default='/etc/group',
+                    help='Path to the group file. Defaults to /etc/group if not provided.')
 parser.add_argument("-s", "--sorted", action='store_true', help='Sort keys of the JSON string alphabetically')
-parser.add_argument("-c", "--compact", action='store_true', help='Print the JSON string in a compact form. The default setting is to pretty-print the JSON.')
+parser.add_argument("-c", "--compact", action='store_true',
+                    help='Print the JSON string in a compact form. The default setting is to pretty-print the JSON.')
 args = parser.parse_args()
 
 passwd_path = args.passwd
 group_path = args.group
 
-verify_file_readable(passwd_path)
-verify_file_readable(group_path)
+try:
+    passwd_file = open(passwd_path)
+    group_file = open(group_path)
+except Exception as e:
+    sys.exit(str(e))
 
-user2groups = parse_group_file(group_path)
-parsed_data = parse_passwd_file(passwd_path, user2groups)
+user2groups = parse_group_file(group_file)
+parsed_data = parse_passwd_file(passwd_file, user2groups)
 parsed_json = json.dumps(parsed_data, indent=None if args.compact else 4, sort_keys=args.sorted)
 
 print(parsed_json)
--- test_passwd_parser.py
+++ test_passwd_parser.py
@@ -1,18 +1,117 @@
+import io
 import unittest
 import passwd_parser as parser
 
 
 class TestPasswdParser(unittest.TestCase):
     def test_validate_group(self):
+        # entries with incorrect number of fields should cause failure
         with self.assertRaises(parser.FileFormatError):
-            parser.validate_group_format("operator:x:37::".split(), 26)
-            parser.validate_group_format("::::".split(), 1)
-            parser.validate_passwd_format("", 1)
+            parser.validate_group_format("operator:x:37::".split(":"), 26, set())
+
+        with self.assertRaises(parser.FileFormatError):
+            parser.validate_group_format("::::".split(":"), 1, set())
+
+        with self.assertRaises(parser.FileFormatError):
+            parser.validate_group_format("".split(":"), 1, set())
+
+        # entries with duplicate groups should cause failure
+        with self.assertRaises(parser.FileFormatError):
+            parser.validate_group_format("whoopsie:x:112:".split(":"), 1, {"whoopsie"})
+
+        # valid entries should not raise an exception
+        try:
+            parser.validate_group_format("scanner:x:118:saned".split(":"), 1, set())
+        except parser.FileFormatError:
+            self.fail("FileFormatException unexpectedly raised for correct input.")
 
     def test_validate_passwd(self):
+        # entries with incorrect number of fields should cause failure
         with self.assertRaises(parser.FileFormatError):
-            parser.validate_passwd_format("www-data:x:33:33:www-data:/var/www:/usr/sbin/nologin:", 13)
-            parser.validate_passwd_format("", 1)
+            parser.validate_passwd_format("www-data:x:33:33:www-data:/var/www:/usr/sbin/nologin:".split(":"), 13, set())
+
+        with self.assertRaises(parser.FileFormatError):
+            parser.validate_passwd_format("".split(":"), 1, set())
+
+        # entries with duplicate users should cause failure
+        with self.assertRaises(parser.FileFormatError):
+            parser.validate_passwd_format("whoopsie:x:112:117::/nonexistent:/bin/false".split(":"), 1, {"whoopsie"})
+
+        # valid entries should not raise an exception
+        try:
+            parser.validate_passwd_format("whoopsie:x:112:117::/nonexistent:/bin/false".split(":"), 1, set())
+        except parser.FileFormatError:
+            self.fail("FileFormatException unexpectedly raised for correct input.")
+
+    def test_parse_group_file(self):
+        mock_group_file = io.StringIO(
+            "whoopsie:x:117:\n" +
+            "scanner:x:118:saned\n" +
+            "saned:x:119:\n" +
+            "pulse:x:120:saned,www-data\n" +
+            "pulse-access:x:121:\n" +
+            "avahi:x:122:\n" +
+            "colord:x:123:")
+
+        mock_parse = {
+            "saned": ["scanner", "pulse"],
+            "www-data": ["pulse"]
+        }
+
+        self.assertEqual(mock_parse, parser.parse_group_file(mock_group_file))
+
+    def test_parse_passwd_file(self):
+        mock_passwd_file = io.StringIO("whoopsie:x:112:117::/nonexistent:/bin/false\n" +
+                                       "kernoops:x:113:65534:Kernel Oops Tracking Daemon,,,:/:/usr/sbin/nologin\n" +
+                                       "saned:x:114:119::/var/lib/saned:/usr/sbin/nologin\n" +
+                                       "pulse:x:115:120:PulseAudio daemon,,,:/var/run/pulse:/usr/sbin/nologin\n" +
+                                       "avahi:x:116:122:Avahi mDNS daemon,,,:/var/run/avahi-daemon:/usr/sbin/nologin\n" +
+                                       "colord:x:117:123:colord colour management daemon,,,:/var/lib/colord:/usr/sbin/nologin")
+
+        mock_user2groups = {
+            "saned": ["scanner", "pulse"],
+            "www-data": ["pulse"]
+        }
+
+        mock_parse = {
+            "whoopsie": {
+                "uid": "112",
+                "full_name": "",
+                "groups": []
+            },
+
+            "kernoops": {
+                "uid": "113",
+                "full_name": "Kernel Oops Tracking Daemon",
+                "groups": []
+            },
+
+            "saned": {
+                "uid": "114",
+                "full_name": "",
+                "groups": ["scanner", "pulse"]
+            },
+
+            "pulse": {
+                "uid": "115",
+                "full_name": "PulseAudio daemon",
+                "groups": []
+            },
+
+            "avahi": {
+                "uid": "116",
+                "full_name": "Avahi mDNS daemon",
+                "groups": []
+            },
+
+            "colord": {
+                "uid": "117",
+                "full_name": "colord colour management daemon",
+                "groups": []
+            },
+        }
+
+        self.assertEqual(mock_parse, parser.parse_passwd_file(mock_passwd_file, mock_user2groups))
 
 
 if __name__ == '__main__':
